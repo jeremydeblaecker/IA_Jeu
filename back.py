456ec921853e3b902bc0e3d5039199748ca17928
jeremydeblaecker <jeremy.deblaecker@gmail.com>
2021-05-10 09:48:33 +0200

    Modification commentaires

--- back.py
+++ back.py
@@ -5,10 +5,10 @@ def backened(depart, obstacles, destination):
     def distance_min(dist, sp_set):                     # chosi un sommet parmis ceux adjacent au parent
         min = 10**10
         global min_index
-        for v in range(400):                            # le point adjacent minimum est choisi
-            if sp_set[v] == False and dist[v]<=min:
-                min = dist[v]
-                min_index = v
+        for points in range(400):                            # le point adjacent minimum est choisi
+            if sp_set[points] == False and dist[points]<=min:
+                min = dist[points]
+                min_index = points
         return min_index
 
     graph, taille = copy.deepcopy(points_adjacent.return_matrix())
@@ -27,10 +27,10 @@ def backened(depart, obstacles, destination):
         u = distance_min(dist, sp_set)                  # return le sommet adjacent le plus proche
         sp_set[u] = True
                                                         # trouve tous les sommets adjacents à celui selectionne
-        for v in range(taille):
-            if sp_set[v] is False and graph[u][v] != 0 and dist[u] != 10**10 and dist[u] + graph[u][v] < dist[v]:
-                dist[v] = dist[u] + graph[u][v]
-                parent[v] = u
+        for points in range(taille):
+            if sp_set[points] is False and graph[u][points] != 0 and dist[u] != 10**10 and dist[u] + graph[u][points] < dist[points]:
+                dist[points] = dist[u] + graph[u][points]
+                parent[points] = u
 
 
     def ancetre(dest):
